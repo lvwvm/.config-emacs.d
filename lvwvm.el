34571e46fb6a387b55895733c9aeb93021e704da
Idigo Luwum <luwum@pm.me>
2020-12-08 11:20:43 -0600

    chore: emacs package description

---
+++ lvwvm.el
@@ -0,0 +1,25 @@
+;;; lvwvm.el --- Further down the hole -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2020 Idigo Luwum
+;;
+;; Author: Idigo Luwum <http://github.com/lvwvm>
+;; Maintainer: Idigo Luwum <luwum@pm.me>
+;; Created: December 08, 2020
+;; Modified: December 08, 2020
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/luwum/lvwvm
+;; Package-Requires: ((emacs 27.1) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Away we go...
+;;
+;;; Code:
+
+
+
+(provide 'lvwvm)
+;;; lvwvm.el ends here
